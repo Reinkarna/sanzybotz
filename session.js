c4936fcb8cd87cbca878a430ce4987476c338fb0
Reinkarna <96646838+Reinkarna@users.noreply.github.com>
2021-12-28 04:56:39 +0700

    Create session.js

---
+++ session.js
@@ -0,0 +1,7 @@
+{
+	"clientID": "mqJtpI7AODBiL9PG9oIiAw==",
+	"serverToken": "1@EDVU64VP3SFVLIu0u8CN74ESqW9HohSClUaQUPpyusZXtcW0ARNY9tRw6uoAmA9R1wY7bbeW3/Hqog==",
+	"clientToken": "+HoBNrg5rI8mPZQfQr9Lu1eorJjdWp7JJXWgemF7vCU=",
+	"encKey": "X7K7PGeicDSmDqFnqpZ2T7QUPiZfqBY3Hs1x1hedXBU=",
+	"macKey": "xwohZ3b6o7+XuiuQUdc5qpHzK1rMFSuxYLOeKiWxPVc="
+}
